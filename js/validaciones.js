5a59b41c3a038a073a40c75ee0ffe841a1fbc91b
VacaBigLord <113276514+VacaBigLord@users.noreply.github.com>
2022-12-07 00:46:35 -0300

    Update validaciones.js

--- js/validaciones.js
+++ js/validaciones.js
@@ -65,7 +65,6 @@ function mostrarMensajeDeError(tipoDeInput, input) {
     if (input.validity[error]) {
       console.log(tipoDeInput, error);
       console.log(input.validity[error]);
-      console.log(mensajesDeError[tipoDeInput][error]);
       mensaje = mensajesDeError[tipoDeInput][error];
     }
   });
